1b095b7eb128c65af839871e197c08d4da08e4df
Jérôme Arnoux <arnoux.jeromepj@gmail.com>
2025-08-25 12:15:06 +0200

    Refactor `utils.py`: improve docstrings for clarity, enhance formatting and argument parsing, rename methods for consistency, and optimize gene family comparison logic.

--- panorama/compare/utils.py
+++ panorama/compare/utils.py
@@ -1,12 +1,20 @@
 #!/usr/bin/env python3
 # coding:utf-8
 
+"""
+A collection of utility functions for pangenome comparison.
+
+This module contains functionalities to compute gene family relatedness, cluster gene families
+based on graph metrics, and launch comparative workflows for pangenome analysis. It also
+includes utilities for building argument parsers related to these workflows.
+"""
+
 # default libraries
 from __future__ import annotations
 import logging
 from pathlib import Path
 import tempfile
-from typing import Any, List, Set, Tuple
+from typing import Any, Dict, List, Set, Tuple
 from multiprocessing import Manager, Lock
 
 # installed libraries
@@ -16,48 +24,137 @@ import networkx as nx
 from panorama.pangenomes import Pangenomes
 from panorama.geneFamily import GeneFamily
 from panorama.format.read_binaries import load_pangenomes
-from panorama.alignment.cluster import cluster_gene_families, write_clustering, parser_mmseqs2_cluster
+from panorama.alignment.cluster import (
+    cluster_gene_families,
+    write_clustering,
+    parser_mmseqs2_cluster,
+)
 
 
-def compute_frr(queries: Set[GeneFamily], targets: Set[GeneFamily]) -> Tuple[float, float, int]:
-    akins = {query_gf.akin.ID for query_gf in queries for target_gf in targets if query_gf.akin == target_gf.akin}
+def compute_gfrr(
+    queries: Set[GeneFamily], targets: Set[GeneFamily]
+) -> Tuple[float, float, int]:
+    """
+    Computes the Gene Family Relatedness Relationship (GFRR) metrics based on the queries and targets
+    gene families. The function evaluates the overlap of 'akin' relationships between the two sets
+    of gene families and calculates both the minimum and maximum FRR values. Additionally, the total
+    count of reciprocal relationships is returned.
+
+    Args:
+        queries (Set[GeneFamily]): A set of query gene families to analyze.
+        targets (Set[GeneFamily]): A set of target gene families to compare against.
+
+    Returns:
+        Tuple[float, float, int]: A tuple containing:
+            - Minimum FRR value (float).
+            - Maximum FRR value (float).
+            - Number of reciprocal relationships (int).
+    """
+    akins = {
+        query_gf.akin.ID
+        for query_gf in queries
+        for target_gf in targets
+        if query_gf.akin == target_gf.akin
+    }
     min_frr = len(akins) / min(len(queries), len(targets))
     max_frr = len(akins) / max(len(queries), len(targets))
 
     return min_frr, max_frr, len(akins)
 
 
-def cluster_on_frr(graph: nx.Graph, frr_metrics: str) -> List[Set[Any]]:
-    partitions = nx.algorithms.community.louvain_communities(graph, weight=frr_metrics)
+def cluster_on_gfrr(graph: nx.Graph, gfrr_metrics: str) -> List[Set[Any]]:
+    """
+    Clusters nodes in a given graph using the Louvain community detection algorithm based
+    on the specified GFRR metric, and assigns clusters as node attributes.
+
+    The function calculates partitions of the input graph, assigns each node to a cluster,
+    and logs the number of clusters created. Each node is updated with a cluster index attribute.
+
+    Args:
+        graph (nx.Graph): The input graph to perform clustering on.
+        gfrr_metrics (str): The edge weight metric to use for clustering.
+
+    Returns:
+        List[Set[Any]]: A list of sets, where each set contains the nodes in a cluster.
+    """
+    partitions = nx.algorithms.community.louvain_communities(graph, weight=gfrr_metrics)
 
     # Add partition index in node attributes
     for i, cluster_nodes in enumerate(partitions):
-        nx.set_node_attributes(graph, {node: f"cluster_{i}" for node in cluster_nodes}, name=f"{frr_metrics}_cluster")
+        nx.set_node_attributes(
+            graph,
+            {node: f"cluster_{i}" for node in cluster_nodes},
+            name=f"{gfrr_metrics}_cluster",
+        )
 
-    logging.info(f"Graph has {len(partitions)} clusters using {frr_metrics}")
+    logging.info(f"Graph has {len(partitions)} clusters using {gfrr_metrics}")
     return partitions
 
 
-def common_launch(args, check_func, need_info: dict, **kwargs) -> Tuple[Pangenomes, Path, Manager, Lock]:
+def common_launch(
+    args, check_func, need_info: Dict[str, Any], **kwargs
+) -> Tuple[Pangenomes, Path, Manager, Lock]:
+    """
+    Common part to launch comparative workflow, handling inputs, resources, and execution logic.
+
+    Args:
+        args: An object containing parsed command-line arguments or input parameters required
+            for the process workflow, including files, computation settings, and configurations.
+        check_func: A function to validate input pangenomes or execute pre-checks during the
+            loading or clustering process.
+        need_info (Dict[str, Any]): A dictionary specifying any additional metadata or information
+            required, such as flags for needing sequences or families information.
+        **kwargs: Additional keyword arguments passed to resource-loading or execution
+            functions for custom configurations.
+
+    Returns:
+        Tuple[Pangenomes, Path, Manager, Lock]: Returns a tuple consisting of the processed
+        pangenomes object for workflow computations, the path to the temporary directory
+        used during processing, a multiprocessing Manager for managing shared memory
+        resources, and a Lock for maintaining thread-safe operations throughout the process.
+    """
     manager = Manager()
     lock = manager.Lock()
     if args.cluster is None:
         need_info["need_families_sequences"] = True
-    pangenomes = load_pangenomes(pangenome_list=args.pangenomes, need_info=need_info,
-                                 check_function=check_func, max_workers=args.cpus, lock=lock,
-                                 disable_bar=args.disable_prog_bar, **kwargs)
+    pangenomes = load_pangenomes(
+        pangenome_list=args.pangenomes,
+        need_info=need_info,
+        check_function=check_func,
+        max_workers=args.cpus,
+        lock=lock,
+        disable_bar=args.disable_prog_bar,
+        **kwargs,
+    )
     tmpdir = Path(tempfile.mkdtemp(dir=args.tmpdir))
     if args.cluster is None:
-        mmseqs2_opt = {"max_seqs": args.max_seqs, "min_ungapped": args.min_ungapped,
-                       "comp_bias_corr": args.comp_bias_corr, "sensitivity": args.sensitivity,
-                       "kmer_per_seq": args.kmer_per_seq, "identity": args.clust_identity,
-                       "coverage": args.clust_coverage, "cov_mode": args.clust_cov_mode, "eval": args.eval,
-                       "max_seq_len": args.max_seq_len, "max_reject": args.max_reject, "align_mode": args.align_mode,
-                       "clust_mode": args.clust_mode, "reassign": args.reassign}
+        mmseqs2_opt = {
+            "max_seqs": args.max_seqs,
+            "min_ungapped": args.min_ungapped,
+            "comp_bias_corr": args.comp_bias_corr,
+            "sensitivity": args.sensitivity,
+            "kmer_per_seq": args.kmer_per_seq,
+            "identity": args.clust_identity,
+            "coverage": args.clust_coverage,
+            "cov_mode": args.clust_cov_mode,
+            "eval": args.eval,
+            "max_seq_len": args.max_seq_len,
+            "max_reject": args.max_reject,
+            "align_mode": args.align_mode,
+            "clust_mode": args.clust_mode,
+            "reassign": args.reassign,
+        }
 
-        clust_res = cluster_gene_families(pangenomes=pangenomes, method=args.method, mmseqs2_opt=mmseqs2_opt,
-                                          tmpdir=tmpdir, keep_tmp=args.keep_tmp, threads=args.cpus, lock=lock,
-                                          disable_bar=args.disable_prog_bar)
+        clust_res = cluster_gene_families(
+            pangenomes=pangenomes,
+            method=args.method,
+            mmseqs2_opt=mmseqs2_opt,
+            tmpdir=tmpdir,
+            keep_tmp=args.keep_tmp,
+            threads=args.cpus,
+            lock=lock,
+            disable_bar=args.disable_prog_bar,
+        )
 
         cluster = tmpdir / "pangenome_gf_clustering.tsv"
         write_clustering(clust_res, cluster)
@@ -70,43 +167,100 @@ def common_launch(args, check_func, need_info: dict, **kwargs) -> Tuple[Pangenom
 
 def parser_comparison(parser):
     """
-    Parser for specific argument of annot command
+    Parser for a specific argument of annot command
 
     :param parser: parser for annot argument
     """
-    required = parser.add_argument_group(title="Required arguments",
-                                         description="All of the following arguments are required :")
-    required.add_argument('-p', '--pangenomes', required=True, type=Path, nargs='?',
-                          help='A list of pangenome .h5 files in .tsv file')
+    required = parser.add_argument_group(
+        title="Required arguments",
+        description="All of the following arguments are required :",
+    )
+    required.add_argument(
+        "-p",
+        "--pangenomes",
+        required=True,
+        type=Path,
+        nargs="?",
+        help="A list of pangenome .h5 files in .tsv file",
+    )
 
-    required.add_argument('-o', '--output', required=True, type=Path,
-                          help="Output directory where the file(s) will be written")
+    required.add_argument(
+        "-o",
+        "--output",
+        required=True,
+        type=Path,
+        help="Output directory where the file(s) will be written",
+    )
 
     compare_opt = parser.add_argument_group(title="Comparison optional arguments")
-    compare_opt.add_argument('--cluster', required=False, type=Path, nargs='?', default=None,
-                             help="A tab-separated file listing the cluster names, the family IDs")
-    compare_opt.add_argument('--frr_cutoff', required=False, type=tuple, default=(0.5, 0.8), nargs=2,
-                             help="The frr (Families Repertoire Relatedness) is used to assess the similarity between two "
-                                  "elements based on their gene families.\n"
-                                  "\tThe 'min_frr': Computes the number of gene families shared between the two elements "
-                                  "and divides it by the smaller number of gene families among the two elements.\n"
-                                  "\tThe 'max_frr': Computes the number of gene families shared between the two elements "
-                                  "and divides it by the larger number of gene families among the two elements."
-                             )
+    compare_opt.add_argument(
+        "--cluster",
+        required=False,
+        type=Path,
+        nargs="?",
+        default=None,
+        help="A tab-separated file listing the cluster names, the family IDs",
+    )
+    compare_opt.add_argument(
+        "--frr_cutoff",
+        required=False,
+        type=tuple,
+        default=(0.5, 0.8),
+        nargs=2,
+        help="The frr (Families Repertoire Relatedness) is used to assess the similarity between two "
+        "elements based on their gene families.\n"
+        "\tThe 'min_frr': Computes the number of gene families shared between the two elements "
+        "and divides it by the smaller number of gene families among the two elements.\n"
+        "\tThe 'max_frr': Computes the number of gene families shared between the two elements "
+        "and divides it by the larger number of gene families among the two elements.",
+    )
     cluster = parser_mmseqs2_cluster(parser)
-    cluster.description = "MMSeqs2 arguments to cluster gene families if no cluster result given"
-    cluster.add_argument("--method", required=False, type=str, choices=["linclust", "cluster"], default="linclust",
-                         help="Choose MMSeqs2 clustering methods:"
-                              "\t-linclust fast but less sensitive clustering"
-                              "\t-cluster slower but more sensitive clustering")
+    cluster.description = (
+        "MMSeqs2 arguments to cluster gene families if no cluster result given"
+    )
+    cluster.add_argument(
+        "--method",
+        required=False,
+        type=str,
+        choices=["linclust", "cluster"],
+        default="linclust",
+        help="Choose MMSeqs2 clustering methods:"
+        "\t-linclust fast but less sensitive clustering"
+        "\t-cluster slower but more sensitive clustering",
+    )
 
     optional = parser.add_argument_group(title="Optional arguments")
-    optional.add_argument('--graph_formats', required=False, type=str, choices=['gexf', "graphml"], nargs="+",
-                          default=None, help="Format of the output graph.")
-    optional.add_argument("--tmpdir", required=False, type=str, nargs='?', default=Path(tempfile.gettempdir()),
-                          help="directory for storing temporary files")
-    optional.add_argument("--keep_tmp", required=False, default=False, action="store_true",
-                          help="Keeping temporary files (useful for debugging).")
-    optional.add_argument("-c", "--cpus", required=False, nargs='?', type=int, default=1,
-                          help="Number of available threads")
+    optional.add_argument(
+        "--graph_formats",
+        required=False,
+        type=str,
+        choices=["gexf", "graphml"],
+        nargs="+",
+        default=None,
+        help="Format of the output graph.",
+    )
+    optional.add_argument(
+        "--tmpdir",
+        required=False,
+        type=str,
+        nargs="?",
+        default=Path(tempfile.gettempdir()),
+        help="directory for storing temporary files",
+    )
+    optional.add_argument(
+        "--keep_tmp",
+        required=False,
+        default=False,
+        action="store_true",
+        help="Keeping temporary files (useful for debugging).",
+    )
+    optional.add_argument(
+        "-c",
+        "--cpus",
+        required=False,
+        nargs="?",
+        type=int,
+        default=1,
+        help="Number of available threads",
+    )
     return required, compare_opt, optional
